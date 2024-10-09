4009275f823c73e681017346d1b3455bdfea67b1
Robin Singh <79157758+robinsingh1309@users.noreply.github.com>
2024-10-09 19:24:54 +0530

    Update stop_container.sh

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Stop the running container (if any) and remove 
+containerID=$(docker ps -q | head -n 1)
+docker stop $containerID
+docker rm -f $containerID
