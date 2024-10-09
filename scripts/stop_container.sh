72debf4389134dd5bef101d6c5910fdeccf6d130
Robin Singh <79157758+robinsingh1309@users.noreply.github.com>
2024-10-09 20:33:54 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Stop the running container (if any) and remove the first container
-containerID='docker ps | awk -F " " '{Print $1}'
+containerID=$(docker ps -q | head -n 1)
 docker rm -f $containerID
