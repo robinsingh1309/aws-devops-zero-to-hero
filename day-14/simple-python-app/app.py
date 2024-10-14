fe3b30daf4e60fb4b88888e97bd14118b47bd147
Robin Singh <79157758+robinsingh1309@users.noreply.github.com>
2024-10-14 10:05:02 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, This is my first Docker Image!'
+    return 'Hello, This is my first Docker Image on DockerHub!'
 
 if __name__ == '__main__':
     app.run()
